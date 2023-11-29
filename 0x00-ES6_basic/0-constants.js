113bc3f390408ef4bac1a011691c2b6a4a60b27e
Mohammednur <mamez7878@gmail.com>
2023-11-29 11:43:57 +0300

    Constants

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -6,7 +6,7 @@ export function taskFirst() {
 export function getLast() {
     return ' is okay';
 }
-  
+
 export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
