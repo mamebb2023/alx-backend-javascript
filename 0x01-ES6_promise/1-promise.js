b8718cf743480239bc47a36fb21b498952d15527
Mohammednur <mamez7878@gmail.com>
2023-12-01 16:41:54 +0300

    Promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,10 @@
 export default function getFullResponseFromAPI(success) {
   const apiPromise = new Promise((resolve, reject) => {
     if (success) {
-        resolve({
-            status: 200,
-            body: 'Success',
-        });
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
     } else {
       reject(new Error('The Fake API is not working correctly'));
     }
