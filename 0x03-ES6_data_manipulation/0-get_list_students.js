76164d36c83d2f32e36e9aad2d1d7fb5de5285d0
Mohammednur <mamez7878@gmail.com>
2023-12-04 14:15:39 +0300

    Get List Students

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 export default function getListStudents() {
-  const map = [];
-
-  map.push({ id: 1, firstname: 'Guillaume', location: 'San Francisco' });
-  map.push({ id: 2, firstname: 'James', location: 'Columbia' });
-  map.push({ id: 5, firstname: 'Serena', location: 'San Francisco' });
+  const map = [
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 5, firstname: 'Serena', location: 'San Francisco' },
+  ];
 
   return (map);
 }
