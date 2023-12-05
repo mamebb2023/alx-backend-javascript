7040909fc162fff4f85d690209810dd3047ab5ea
Mohammednur <mamez7878@gmail.com>
2023-12-05 10:02:24 +0300

    Get Ids Sum

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,10 +1,10 @@
 export default function getStudentIdsSum(arr) {
   if (Array.isArray(arr)) {
     return arr.reduce((sum, val, i) => {
-        if (i === 1) {
-            return sum.id + val.id;
-        }
-        return sum + val.id;
+      if (i === 1) {
+        return sum.id + val.id;
+      }
+      return sum + val.id;
     });
   }
   return [];
