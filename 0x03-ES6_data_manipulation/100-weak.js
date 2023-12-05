897c0ac692c365db259ce6d7ca5d693ece741dd7
Mohammednur <mamez7878@gmail.com>
2023-12-05 20:57:54 +0300

    Advanced

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,13 @@
 export const weakMap = new WeakMap();
 
-export function queryAPI(endPoint){
-    if (weakMap.has(endPoint)){
-        const counter = weakMap.get(endPoint);
-        if (weakMap.get(endPoint) >= 4){
-            throw Error('Endpoint load is high');
-        }
-        weakMap.set(endPoint, (counter + 1));
-    } else {
-        weakMap.set(endPoint, 1);
+export function queryAPI(endPoint) {
+  if (weakMap.has(endPoint)) {
+    const counter = weakMap.get(endPoint);
+    if (weakMap.get(endPoint) >= 4) {
+      throw Error('Endpoint load is high');
     }
+    weakMap.set(endPoint, (counter + 1));
+  } else {
+    weakMap.set(endPoint, 1);
+  }
 }
