71e8b39185451dfe1c754f37602cd5649cbce365
Mohammednur <mamez7878@gmail.com>
2023-12-05 14:18:41 +0300

    Clean Set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
-export default function cleanSet(set, startString){
-    if (!startString){
-        return '';
+export default function cleanSet(set, startString) {
+  if (!startString) {
+    return '';
+  }
+  const string = [];
+  for (const e of set) {
+    if (e.startsWith(startString)) {
+      string.push(e.slice(startString.length));
     }
-    const string = [];
-    for (const e of set){
-        if (e.startsWith(startString)){
-            string.push(e.slice(startString.length));
-        }
-    }
-    return string.join('-');
+  }
+  return string.join('-');
 }
