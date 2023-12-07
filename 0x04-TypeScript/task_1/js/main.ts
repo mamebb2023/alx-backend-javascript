426ba986cd5e5fb732dcf20ec0d41d359ad10f0e
Mohammednur <mamez7878@gmail.com>
2023-12-07 09:58:17 +0300

    Task 1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,8 @@
+export interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: true | false;
+    yearsOfExperience?: number;
+    location: string;
+    [att: string]: any;
+}
