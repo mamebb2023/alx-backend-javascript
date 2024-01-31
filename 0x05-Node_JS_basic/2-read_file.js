9901ee0c216a4fce3b90503b1ddc419dcfb6e779
Mohammednur <mamebb2023@gmail.com>
2024-01-31 11:09:14 +0300

    Read file

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,28 @@
+const { readFileSync, existsSync } = require('fs');
+
+function countStudents(path) {
+    if (!existsSync(path)) {
+        throw new Error('Cannot load the database');
+    }
+
+    const content = readFileSync(path, 'utf-8').trim().split('\n');
+    console.log(`Number of students: ${content.length - 1}`);
+    const count_n = new Map();
+    for (const row of content.splice(1)) {
+        const tmp = row.split(',');
+        const fieldName = tmp[tmp.length - 1];
+        const studentName = tmp.splice(0, 1).join( );
+
+        if (count_n.has(fieldName)) {
+            count_n.get(fieldName).push(studentName);
+        } else {
+            count_n.set(fieldName, [studentName]);
+        }
+    }
+
+    for (const [fieldName, students] of count_n) {
+        console.log(`Number of students in ${fieldName}: ${students.length}. List: ${students.join(', ')}`);
+    }
+}
+
+module.exports = countStudents;
