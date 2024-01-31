d02f326bd3b7714a6c20533286d9acb3bd2c13af
Mohammednur <mamebb2023@gmail.com>
2024-01-31 15:41:03 +0300

    JS http server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -11,23 +11,21 @@ function countStudents(path) {
 
   const content = readFileSync(path, 'utf8').trim().split('\n');
   let s = `Number of students: ${content.length - 1}\n`;
-  const countHolder = new Map();
+  const fields = new Map();
   for (const row of content.splice(1)) {
     const tmp = row.split(',');
     const fieldName = tmp[tmp.length - 1];
     const studentName = tmp.splice(0, 1).join(' ');
 
-    if (countHolder.has(fieldName)) {
-      countHolder.get(fieldName).push(studentName);
+    if (fields.has(fieldName)) {
+      fields.get(fieldName).push(studentName);
     } else {
-      countHolder.set(fieldName, [studentName]);
+      fields.set(fieldName, [studentName]);
     }
   }
 
-  for (const [fieldName, students] of countHolder) {
-    s += `Number of students in ${fieldName}: ${
-      students.length
-    }. List: ${students.join(', ')}\n`;
+  for (const [fieldName, students] of fields) {
+    s += `Number of students in ${fieldName}: ${students.length}. List: ${students.join(', ')}\n`;
   }
   s = s.slice(0, s.length - 1);
   return s;
