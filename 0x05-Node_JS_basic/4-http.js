7b14c480536135d793bcb19aefe56282a732ac16
Mohammednur <mamebb2023@gmail.com>
2024-01-31 13:42:06 +0300

    NodeJS Http Server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,14 @@
-const http = require('node:http');
+const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-server.listen(port, hostname);
+app.listen(port, hostname);
+
+module.exports = app;
