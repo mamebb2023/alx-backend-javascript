7d7ffffa1a6c9371010b3b87ff6b0606c561d26d
Mohammednur <mamebb2023@gmail.com>
2024-01-31 15:43:21 +0300

    Read File Async

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -7,11 +7,12 @@ function countStudents(path) {
         reject(new Error('Cannot load the database'));
       }
       const content = data.toString('utf-8').trim().split('\n');
+      console.log(`Number of students: ${content.length - 1}`);
       const fields = new Map();
       for (const row of content.splice(1)) {
         const tmp = row.split(',');
         const fieldName = tmp[tmp.length - 1];
-        const studentName = tmp[0];
+        const studentName = tmp.splice(0, 1).join(' ');
 
         if (fields.has(fieldName)) {
           fields.get(fieldName).push(studentName);
@@ -20,7 +21,6 @@ function countStudents(path) {
         }
       }
 
-      console.log(`Number of students: ${content.length - 1}`);
       for (const [fieldName, students] of fields) {
         console.log(`Number of students in ${fieldName}: ${students.length}. List: ${students.join(', ')}`);
       }
