2214d9c3e2105b0b6617504e571d0ba4715cef80
Mohammednur <mamebb2023@gmail.com>
2024-01-31 11:14:03 +0300

    Read file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -7,20 +7,20 @@ function countStudents(path) {
 
   const content = readFileSync(path, 'utf-8').trim().split('\n');
   console.log(`Number of students: ${content.length - 1}`);
-  const count_n = new Map();
+  const fields = new Map();
   for (const row of content.splice(1)) {
     const tmp = row.split(',');
     const fieldName = tmp[tmp.length - 1];
     const studentName = tmp.splice(0, 1).join();
 
-    if (count_n.has(fieldName)) {
-      count_n.get(fieldName).push(studentName);
+    if (fields.has(fieldName)) {
+      fields.get(fieldName).push(studentName);
     } else {
-      count_n.set(fieldName, [studentName]);
+      fields.set(fieldName, [studentName]);
     }
   }
 
-  for (const [fieldName, students] of count_n) {
+  for (const [fieldName, students] of fields) {
     console.log(`Number of students in ${fieldName}: ${students.length}. List: ${students.join(', ')}`);
   }
 }
