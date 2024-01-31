3d77468902d4542f75514f9d610936483f8311d8
Mohammednur <mamebb2023@gmail.com>
2024-01-31 15:32:19 +0300

    JS Http Server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,13 +1,55 @@
 const http = require('http');
+const { readFileSync, existsSync } = require('fs');
 
-const host = '127.0.0.1';
-const port = 1245;
+function getStudentsString(path) {
+  if (!existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+
+  const content = readFileSync(path, 'utf8').trim().split('\n');
+  let finalString = `Number of students: ${content.length - 1}\n`;
+  const countHolder = new Map();
+  for (const row of content.splice(1)) {
+    const tmp = row.split(',');
+    const fieldName = tmp[tmp.length - 1];
+    const studentName = tmp.splice(0, 1).join(' ');
+
+    if (countHolder.has(fieldName)) {
+      countHolder.get(fieldName).push(studentName);
+    } else {
+      countHolder.set(fieldName, [studentName]);
+    }
+  }
+
+  for (const [fieldName, students] of countHolder) {
+    finalString += `Number of students in ${fieldName}: ${
+      students.length
+    }. List: ${students.join(', ')}\n`;
+  }
+  finalString = finalString.slice(0, finalString.length - 1);
+  return finalString;
+}
 
 const app = http.createServer((req, res) => {
-    res.get('/students'. )
-    res.end("Hello Holberton School!");
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  switch (req.url) {
+    case '/':
+      res.end('Hello Holberton School!');
+      break;
+    case '/students':
+      try {
+        const finalString = getStudentsString(process.argv[2]);
+        res.end(`This is the list of our students\n${finalString}`);
+      } catch (error) {
+        res.end(`This is the list of our students\n${error.message}`);
+      }
+      break;
+    default:
+      break;
+  }
 });
 
-
+app.listen(1245, '127.0.0.1');
 
 module.exports = app;
