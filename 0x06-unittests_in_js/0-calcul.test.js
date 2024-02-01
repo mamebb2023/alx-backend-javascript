216413672dbd11395d3ac2f356e94f220e9be536
Mohammednur <mamebb2023@gmail.com>
2024-02-01 14:47:24 +0300

    Calcul Test

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const { it, describe } = require('mocha');
+const calculateNumber = require('./0-calcul');
+
+describe("calculateNumber", () => {
+    it("rounds given two numbers and returns there sum", () => {
+        assert.equal(calculateNumber(1, 3), 4);
+    });
+    it("rounds given two numbers and returns there sum", () => {
+        assert.equal(calculateNumber(1, 3.7), 5);
+    });
+    it("rounds given two numbers and returns there sum", () => {
+        assert.equal(calculateNumber(1.2, 3.7), 5);
+    });
+    it("rounds given two numbers and returns there sum", () => {
+        assert.equal(calculateNumber(1.5, 3.7), 6);
+    });
+    it("rounds given two numbers and returns there sum", () => {
+        assert.equal(calculateNumber(5.4, 4.5), 10);
+    });
+});
