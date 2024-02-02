387ea1097f787092786e80a41785437f2fefb41f
Mohammednur <mamebb2023@gmail.com>
2024-02-02 19:43:08 +0300

    Calcul Chai Test

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,6 +1,7 @@
+const { expect } = require('chai');
 const { describe, it } = require('mocha');
-const expect = require('chai');
-const calculateNumber = require('./1-calcul');
+
+const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', () => {
     describe('Addition', () => {
