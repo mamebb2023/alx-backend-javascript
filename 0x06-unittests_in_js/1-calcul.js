bad93151512e44cb682f8b5ea3a95d50be8e28b2
Mohammednur <mamebb2023@gmail.com>
2024-03-06 13:51:48 +0300

    Calcul and It's Test

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,25 +1,15 @@
-/**
- * Adds, subtracts or divides two numbers depending
- * on operator type specified
- * @param   {string}  type  - SUM, SUBTRACT, or DIVIDE
- * @param   {number}  a
- * @param   {number}  b
- * @returns {number}  -  The result of operation of a and b
- */
 function calculateNumber(type, a, b) {
-  const numOne = Math.round(a);
-  const numTwo = Math.round(b);
-  switch (type) {
-    case 'SUM':
-      return numOne + numTwo;
-    case 'SUBTRACT':
-      return numOne - numTwo;
-    case 'DIVIDE':
-      if (!numTwo) return 'Error';
-      return numOne / numTwo;
-    default:
-      throw new Error('Operation not supported. Type must be SUM, SUBTRACT or DIVIDE');
-  }
+    switch (type) {
+        case 'SUM':
+            return Math.round(a) + Math.round(b);
+        case 'SUBTRACT':
+            return Math.round(a) -  Math.round(b);
+        case 'DIVIDE':
+            if (Math.round(b) === 0) return 'Error';
+            return Math.round(a) / Math.round(b);
+        default:
+            return 0;
+    }
 }
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,38 +1,66 @@
-/**
- * 1-calculus.js.calculateNumber test suite
- */
 const assert = require('assert');
+const { it, describe } = require('mocha');
 const calculateNumber = require('./1-calcul');
 
-describe('calculateNumber nodejs assertion test suite', function() {
-  describe('#SUM operation', function() {
-    it('should return the sum of its rounded arguments', function() {
-      assert.equal(calculateNumber('SUM', 1, 3), 4);
-      assert.equal(calculateNumber('SUM', 1.2, 3.7), 5);
-      assert.equal(calculateNumber('SUM', 1.5, 3.7), 6);
+describe('calculateNumber', () => {
+    describe('Addition', () => {
+        it('rounding 1, 3, with SUM returns 4', () => {
+            assert.equal(calculateNumber('SUM', 1, 3), 4);
+        });
+        it('rounding 1, 3.7, with SUM returns 5', () => {
+            assert.equal(calculateNumber('SUM', 1, 3.7), 5);
+        });
+        it('rounding 1.2, 3.7, with SUM and returns 5', () => {
+            assert.equal(calculateNumber('SUM', 1.2, 3.7), 5);
+        });
+        it('rounding 1.5, 3.7, with SUM and returns 6', () => {
+            assert.equal(calculateNumber('SUM', 1.5, 3.7), 6);
+        });
+        it('rounding 1.4, 4.5, with SUM returns 6', () => {
+            assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+        });
+        it('rounding -1.3, -3.7, with SUM and returns -5', () => {
+            assert.equal(calculateNumber('SUM', -1.3, -3.7), -5);
+        });
     });
-  });
-  describe('#SUBTRACT operation', function() {
-    it('should return the subtraction of its rounded arguments', function() {
-      assert.equal(calculateNumber('SUBTRACT', 1, 3), -2);
-      assert.equal(calculateNumber('SUBTRACT', 3, 2), 1);
-      assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-      assert.equal(calculateNumber('SUBTRACT', 3.7, 1.5), 2);
+    describe('SUBTRACTION', () => {
+        it('rounding 1, 2, with SUBTRACT returns -1', () => {
+            assert.equal(calculateNumber('SUBTRACT', 1, 2), -1);
+        });
+        it('rounding 1, 3.7, with SUBTRACT returns -3', () => {
+            assert.equal(calculateNumber('SUBTRACT', 1, 3.7), -3);
+        });
+        it('rounding 1.2, 3.7, with SUBTRACT and returns -3', () => {
+            assert.equal(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
+        });
+        it('rounding 1.5, 3.7, with SUBTRACT and returns -2', () => {
+            assert.equal(calculateNumber('SUBTRACT', 1.5, 3.7), -2);
+        });
+        it('rounding 1.4, 4.5, with SUM returns -4', () => {
+            assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+        });
+        it('rounding -1.3, -3.7, with SUBTRACT and returns 3', () => {
+            assert.equal(calculateNumber('SUBTRACT', -1.3, -3.7), 3);
+        });
     });
-  });
-  describe('#DIVIDE operation', function() {
-    it('should return the quotient from the division its rounded arguments', function() {
-      assert.equal(calculateNumber('DIVIDE', 3, 1), 3);
-      assert.equal(calculateNumber('DIVIDE', 3.1, 1.5), 1.5);
-      assert.equal(calculateNumber('DIVIDE', 6.5, 1.5), 3.5);
-      assert.equal(calculateNumber('DIVIDE', -3.7, -1.5), 4);
-      assert.equal(calculateNumber('DIVIDE', 3, 0), 'Error');
+    describe('DIVIDION', () => {
+        it('rounding 1, 2, with DIVIDE returns 0.5', () => {
+            assert.equal(calculateNumber('DIVIDE', 1, 2), 0.5);
+        });
+        it('rounding 6, 3.2, with DIVIDE returns 2', () => {
+            assert.equal(calculateNumber('DIVIDE', 6, 3.2), 2);
+        });
+        it('rounding 3.1, 0.5, with DIVIDE and returns 3', () => {
+            assert.equal(calculateNumber('DIVIDE', 3.1, 0.5), 3);
+        });
+        it('rounding 3, -2.7, with DIVIDE and returns -1', () => {
+            assert.equal(calculateNumber('DIVIDE', 3, -2.7), -1);
+        });
+        it('rounding 1.4, 4.5, with SUM returns 0.2', () => {
+            assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+        });
+        it('rounding -1.3, 0, with DIVIDE and returns "Error"', () => {
+            assert.equal(calculateNumber('DIVIDE', -1.3, 0), 'Error');
+        });
     });
-  });
-  describe('#INVALID operation', function() {
-    it('should throw an exception for invalid type', function() {
-      assert.throws(calculateNumber.bind('MULTIPLY', 1, 3));
-      assert.throws(calculateNumber.bind(false, 1, 2));
-    });
-  });
 });
